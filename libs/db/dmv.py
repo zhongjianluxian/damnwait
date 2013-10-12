224306451ddcff1f12965d8ab08ef5295b3f007b
Wei Hou <weihou.usc@gmail.com>
2013-10-11 22:06:17 -0500

    Converting tabs into spaces and adding a new line at the end of some files

--- libs/db/dmv.py
+++ libs/db/dmv.py
@@ -1,12 +1,12 @@
 from google.appengine.ext import db
 
 class DMV(db.Model):
-	name = db.StringProperty(required = True)
-	mnemonic = db.StringProperty(required = True)
-	address = db.PostalAddressProperty(required = True)
-	telephone = db.PhoneNumberProperty(required = True)
-	email = db.EmailProperty()
-	sample_tm = db.DateTimeProperty(auto_now_add = True)
-	wait_time_non_appt = db.FloatProperty()
-	wait_time_appt = db.FloatProperty()
-	
+    name = db.StringProperty(required = True)
+    mnemonic = db.StringProperty(required = True)
+    address = db.PostalAddressProperty(required = True)
+    telephone = db.PhoneNumberProperty(required = True)
+    email = db.EmailProperty()
+    sample_tm = db.DateTimeProperty(auto_now_add = True)
+    wait_time_non_appt = db.FloatProperty()
+    wait_time_appt = db.FloatProperty()
+    
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-    	self.response.headers['Content-Type'] = 'text/plain'
-    	self.response.write('Welcome to damnwait!')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Welcome to damnwait!')
 
 
 
