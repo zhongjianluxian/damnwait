4eb1db6b0b44ed3493ea16294fdaecef041e169c
Chen Xie <chenxie.dev@gmail.com>
2013-10-15 14:48:05 -0700

    add support for all dmv offices in the json file

--- handlers/hackdmv.py
+++ handlers/hackdmv.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 from pytz import timezone
 from datetime import datetime
 from db import dmv
+import json
 
 class HackDMV(webapp2.RequestHandler):
 
@@ -18,32 +19,37 @@ class HackDMV(webapp2.RequestHandler):
     
     def get(self):
 
+        #read the dmv_info file
+        f = open(os.path.dirname(__file__)+"/dmv_info.json")
+        dmv_list = json.load(f)
+        id_list = [ int(dmv['dmv_id']) for dmv in dmv_list ]
+
         #extract wait time information
-        id = "548"
-        soup = BeautifulSoup(urllib2.urlopen("http://apps.dmv.ca.gov/fo/offices/appl/fo_data_read.jsp?foNumb=%s" %id))
-        if "display:block" in soup('div', {'id':'showClosedDiv'})[0]['style']:
-            tz = "America/Los_Angeles"
-            local_timezone = timezone("%s" %tz)
-            local_time = local_timezone.localize(datetime.today())
-            
-            memcache.set("sample_local_time", local_time)
+        for id in id_list:
+            soup = BeautifulSoup(urllib2.urlopen("http://apps.dmv.ca.gov/fo/offices/appl/fo_data_read.jsp?foNumb=%s" %id))
+            if "display:block" in soup('div', {'id':'showClosedDiv'})[0]['style']:
+                tz = "America/Los_Angeles"
+                local_timezone = timezone("%s" %tz)
+                local_time = local_timezone.localize(datetime.today())
+                
+                memcache.set("sample_local_time", local_time)
 
-        else:
-            appt_wait_time = self.convertToMinutes(str(soup('span',{'id':'apptWaitTime'})[0].contents[0]))
-            nonappt_wait_time = self.convertToMinutes(str(soup('span',{'id':'nonApptWaitTime'})[0].contents[0]))
-            
-            #insert data into data store
-            t = dmv.DMV( dmv_id = id,
-                sample_tm = datetime.now(),
-                non_appt_wait_mins = nonappt_wait_time,
-                appt_wait_mins = appt_wait_time
-                )
-            
-            t.put()
+            else:
+                appt_wait_time = self.convertToMinutes(str(soup('span',{'id':'apptWaitTime'})[0].contents[0]))
+                nonappt_wait_time = self.convertToMinutes(str(soup('span',{'id':'nonApptWaitTime'})[0].contents[0]))
+                
+                #insert data into data store
+                t = dmv.DMV( dmv_id = id,
+                    sample_tm = datetime.now(),
+                    non_appt_wait_mins = nonappt_wait_time,
+                    appt_wait_mins = appt_wait_time
+                    )
+                
+                t.put()
 
-            #for cron job testing purpose
-            memcache.set("appt_wait_time", appt_wait_time)
-            memcache.set("nonappt_wait_time", nonappt_wait_time)
+                #for cron job testing purpose
+                memcache.set("appt_wait_time", appt_wait_time)
+                memcache.set("nonappt_wait_time", nonappt_wait_time)
 
 
 
