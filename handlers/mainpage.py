d2e3e6cf93f947141b07b8abb0555671d6a13ae1
Chen Xie <chenxie.dev@gmail.com>
2013-10-15 00:54:34 -0700

    added template for testing purpose

--- handlers/mainpage.py
+++ handlers/mainpage.py
@@ -1,7 +1,15 @@
 import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/../templates"),
+    extensions=['jinja2.ext.autoescape'])
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Welcome to damnwait!\n\n')
-        self.response.write('Enjoy this site! ^_^')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('<h1>Welcome to damnwait!</h1>')
+
+        template = JINJA_ENVIRONMENT.get_template("main.html")
+        self.response.write(template.render())
