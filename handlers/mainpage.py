d3b4826e7476b1819c4ffe7fb7953c4b2e025474
Chen Xie <chenxie.dev@gmail.com>
2013-10-17 22:34:25 -0700

    Revert mainpage.py

---
+++ handlers/mainpage.py
@@ -0,0 +1,12 @@
+import webapp2
+import jinja2
+import os
+
+from utils import render_str
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        # self.response.write('<h1>Welcome to damnwait!</h1>')
+
+        self.response.write(render_str("main.html"))
