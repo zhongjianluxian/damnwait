8c213dc1a735076b2ed88baba977c87a91cf2950
Chen Xie <chenxie.dev@gmail.com>
2013-10-20 15:21:54 -0700

    resize image for test

--- handlers/mainpage.py
+++ handlers/mainpage.py
@@ -14,13 +14,14 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
         
-        G = Line('cEAELFJHUc',encoding='simple')
+        G = Line('abcdefghijklmnopqrstsrqponmlkjihgf',encoding='simple')
         G.color('76A4FB')
         G.line(2)
         G.axes('x')
+        G.size(600,300)
         G.axes.range(0,10,50,5)
 
         template_values = {
-			"chart_src": "http://chart.apis.google.com/chart?chd=s:cEAELFJHUc&chs=300x150&cht=lc"
+			"chart_src": str(G)
 			}
         self.response.write(render_str("home.html", template_values))
