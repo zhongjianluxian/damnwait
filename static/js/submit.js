bc5f5f8e29349430f543b3f3adbc9a5ed82c63e4
Chen Xie <chenxie.dev@gmail.com>
2013-10-23 15:49:01 -0700

    Sample submit request javascript to start with

---
+++ static/js/submit.js
@@ -0,0 +1,16 @@
+<script language='javascript'>
+function submit()
+{
+var xmlhttp=new XMLHttpRequest();
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("data").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("POST","/submit",true);
+xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+xmlhttp.send("fname=chen&lname=xie");
+}
+</script>
