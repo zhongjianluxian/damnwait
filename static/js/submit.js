409ba782e7895b9a1aac2a86694cf643fd9b18c1
Chen Xie <chenxie.dev@gmail.com>
2013-10-25 01:48:11 -0700

    minor change of page transition and parameter passing

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,6 +1,11 @@
 
-function submit()
+function submit(weekday)
 {
+  if(typeof(weekday)==="undefined")
+    weekday = 4;
+  var select = document.getElementById("office");
+  var option = select.options[select.selectedIndex].id;
+  var input = document.getElementById("mon");
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function()
   	{
@@ -11,6 +16,6 @@ function submit()
   	}
 	xmlhttp.open("POST","/submit/",true);
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xmlhttp.send();
+  xmlhttp.send("office="+option+"&weekday="+weekday);
 }
 
