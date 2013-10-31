a85f940ef2692fe2dc73e87a13184cd9dd59efb6
Chen Xie <chenxie.dev@gmail.com>
2013-10-31 00:37:27 -0700

    Added query results to chart

--- handlers/mainpage.py
+++ handlers/mainpage.py
@@ -40,22 +40,33 @@ class WaitTimeQuery(webapp2.RequestHandler):
         weekday = self.request.get("weekday")
         
         q = dmv.DMV.query().filter(ndb.GenericProperty('dmv_id') == office).filter(ndb.GenericProperty('weekday') == int(weekday))
-        a= q.fetch()
+        rst = q.fetch()
 
-        G = Line('jkfdsfjhsdjfhskfhdskjhaskldhsakjldhdhakjhdwhwjhkjawhdjkahdjk',encoding='simple')
+        node_appt = []
+        node_nonappt = []
+        node_sample_tm = []
+
+        for r in rst:
+            node_appt.append(int(r['appt_wait_mins']))
+            node_nonappt.append(int(r['non_appt_wait_mins']))
+            node_sample_tm.append(r['sample_tm'])
+
+        G = LineXY( [ 
+                   node_sample_tm, # x values
+                   node_appt, # y values, etc.
+                   node_nonappt,
+                   ['-1'], # domain not found, interpolated
+                  ])
         G.color('76A4FB')
         G.line(4)
         G.size(600, 300)
         G.axes('xy')
         G.axes.range(0,8,17,1)
         G.axes.range(1,0,200,50)
-        G.axes.label(0, '8AM', '9', '10', '11', '12PM', '1', '2', '3', '4', '5PM')
+        #G.axes.label(0, '8AM', '9', '10', '11', '12PM', '1', '2', '3', '4', '5PM')
         G.marker('fMax','red',0,19,10)
         G.grid(8,17,1,0)
 
-        template_values = {
-                        "chart_src": str(G)
-                        }
         self.response.write(str(G))
 
 
